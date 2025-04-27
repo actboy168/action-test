483296170f05ef23c08690b2487815e2f6af34a2
actboy168 <actboy168@gmail.com>
2025-04-27 12:13:29 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,6 +1,16 @@
-local function test()
-  local f <close> = assert(io.popen("sysctl sysctl.proc_translated", "r"))
-  return f:read "l"
+local function shell(command)
+    local f = assert(io.popen(command, "r"))
+    local r = f:read "l"
+    f:close()
+    return r
 end
 
-print(test())
+local function get_env(name)
+    name = "%"..name.."%"
+    local value = shell("echo "..name)
+    if value == name then
+        return ""
+    end
+    return value
+end
+print(get_env "PROCESSOR_ARCHITECTURE")
